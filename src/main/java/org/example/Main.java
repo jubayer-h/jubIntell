4ea1c0c6e8c7edb438c9760fdfcc043a725169c1
jubayerh <jubayer@hotmail.com>
2023-11-15 20:20:06 -0500

    3first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    int a;
+    static void test1(){
+        System.out.println("This is test");
     }
+     static void test(){
+        System.out.println("this is test");
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        test();
+        test();
+    }
+
 }
