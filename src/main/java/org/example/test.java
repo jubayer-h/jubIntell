f59b2f746cea3128741410a7c716c43dd08e78bd
jubayerh <jubayer@hotmail.com>
2023-11-15 20:28:37 -0500

    4 new class & code

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -2,4 +2,14 @@ package org.example;
 
 public class test {
     //this is another class
+    void test1(){
+
+        System.out.println("hello this another class");
+    }
+
+    public static void main(String[] args) {
+        test t=new test();
+        t.test1();
+    }
+
 }
