ce912b1970084729fbf1b7e83ec9a8b2f532e8e0
jubayerh <jubayer@hotmail.com>
2023-11-16 09:41:36 -0500

    1-new class

--- src/main/java/org/example/mobile.java
+++ src/main/java/org/example/mobile.java
@@ -4,6 +4,6 @@ public class mobile {
 
 
     public static void main(String[] args) {
-        System.out.println("thsi is class is moble");
+        System.out.println("thsi is class is moble");//
     }
 }
